365323593615444a5ec3263eae1f3b943c11172e
Joakim Karlsson <joakim.t.karlsson.1985@gmail.com>
2022-12-12 22:36:33 +0100

    fix typo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const fetchUsers = async () =>
   //   await (await fetch("http://localhost:9000/getusers")).json();
-  await (await fetch("/.netlify/functions/getuser")).json();
+  await (await fetch("/.netlify/functions/getusers")).json();
 
 fetchUsers().then((data) => {
   //   userList = document.querySelector("#users");
